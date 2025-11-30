ef56c25947478235a3e07d43ba25dd79633c2102
CardinalJV <129071629+CardinalJV@users.noreply.github.com>
2025-11-30 18:43:52 +0100

    Initial Commit

---
+++ SampleUserNotifications/SampleUserNotificationsApp.swift
@@ -0,0 +1,17 @@
+//
+//  SampleUserNotificationsApp.swift
+//  SampleUserNotifications
+//
+//  Created by Viranaiken Jessy on 30/11/25.
+//
+
+import SwiftUI
+
+@main
+struct SampleUserNotificationsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
